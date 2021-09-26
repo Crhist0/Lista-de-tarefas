7018e62fb606cb9f62c0d65ac2bab034326e3d83
Crhistian de Oliveira <88751764+Crhist0@users.noreply.github.com>
2021-09-26 18:00:17 -0300

    Add files via upload
    pequenos ajustes

--- taskList.js
+++ taskList.js
@@ -148,7 +148,7 @@ function showList() {
             '<div class="modal fade" id="editModal' +
             y +
             '" tabindex="-1" aria-labelledby="exampleModalLabel"aria-hidden="true">' +
-            '<div class="modal-dialog"><div class="modal-content">' +
+            '<div class="modal-dialog modal-xl"><div class="modal-content">' +
             '<div class="modal-header">' +
             '<h5 class="modal-title" id="exampleModalLabel">Editar tarefa</h5>' +
             '<button type="button" class="btn-close" data-bs-dismiss="modal"aria-label="Close"></button>' +
